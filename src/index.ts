11b33fac071886944b65b0d53b7cc09d2a84ad3c
Arkadiusz Krauzowicz <40127987+arktosk@users.noreply.github.com>
2024-06-29 16:18:17 +0200

    Real minimal epub file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import AdmZip from "adm-zip";
 import fs from "node:fs/promises";
 import { existsSync } from "node:fs";
 import path from "node:path";
+import AdmZip from "adm-zip";
 
-const archive = new AdmZip();
+const archive = new AdmZip(undefined, { noSort: true });
 
+// Mimetype file entry must be the first file in the archive and has no compression
 archive.addFile("mimetype", Buffer.from("application/epub+zip"));
+archive.getEntry("mimetype")!.header.method = 0; // Disabling compression for particular file
 
 archive.addFile(
     "META-INF/container.xml",
@@ -35,32 +37,33 @@ archive.addFile(
     <meta property="dcterms:modified">${modifiedAt}</meta>
   </metadata>
   <manifest>
-    <item id="intro" href="intro.xhtml" media-type="application/xhtml+xml" />
-    <item id="nav" href="nav.xhtml" media-type="application/xhtml+xml" properties="nav"/>
+    <item id="nav" href="nav.xhtml" media-type="application/xhtml+xml" properties="nav"/><!-- required -->
   </manifest>
   <spine>
-    <itemref idref="intro" />
+    <itemref idref="nav" /><!-- not required, but it is the only file -->
   </spine>
 </package>`)
 );
 
+// <meta name="dtb:uid" content="${identifier}"/>
+// <meta name="dtb:depth" content="1"/>
+// <!-- <meta name="dtb:generator" content="EBOOK Creator"/> -->
+// <meta name="dtb:totalPageCount" content="0"/>
+// <meta name="dtb:maxPageNumber" content="0"/>
+const tocTitle = "Table of Contents";
 archive.addFile(
     "EPUB/nav.xhtml",
     Buffer.from(`<?xml version='1.0' encoding='UTF-8'?>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" xml:lang="en">
   <head>
-    <title>${title}</title>
-    <meta name="dtb:uid" content="${identifier}"/>
-    <meta name="dtb:depth" content="1"/>
-    <!-- <meta name="dtb:generator" content="EBOOK Creator"/> -->
-    <meta name="dtb:totalPageCount" content="0"/>
-    <meta name="dtb:maxPageNumber" content="0"/>
+    <title>${tocTitle}</title>
   </head>
   <body epub:type="frontmatter">
     <nav epub:type="toc">
+      <h1>${tocTitle}</h1>
       <ol>
         <li>
-          <a href="intro.xhtml" id="np-1">${title}</a>
+          <a href="nav.xhtml">${tocTitle}</a>
         </li>
       </ol>
     </nav>
@@ -68,20 +71,20 @@ archive.addFile(
 </html>`)
 );
 
-archive.addFile(
-    "EPUB/intro.xhtml",
-    Buffer.from(`<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <title>${title}</title>
-  </head>
-<body>
-  <h1>
-    ${title}
-  </h1>
-</body>
-</html>`)
-);
+// archive.addFile(
+//     "EPUB/intro.xhtml",
+//     Buffer.from(`<!DOCTYPE html>
+// <html xmlns="http://www.w3.org/1999/xhtml">
+//   <head>
+//     <title>${title}</title>
+//   </head>
+// <body>
+//   <h1>
+//     ${title}
+//   </h1>
+// </body>
+// </html>`)
+// );
 
 const archiveBuffer = archive.toBuffer();
 
